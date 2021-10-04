71ea0f9047d93f6f2795e0dc199dd1338d6b9d2a
Treyvion Arte Johnson <tajohns22@gmail.com>
2021-10-03 21:22:06 -0500

    Added mean and median to math controller

---
+++ controllers/mathController.js
@@ -0,0 +1,67 @@
+const ExpressError = require('../helpers/expressError');
+/**
+ * Math controller class for complex calculations
+ */
+class mathController {
+    
+    /**
+     * Calculate the mean of an array of numbers
+     * @param {number} numArr 
+     * @returns number
+     */
+    mean(numArr){
+        // Handle validation
+
+        // sum reducer
+        const sum = (prev, curr) => prev + curr;
+        
+        const length = numArr.length;
+
+        // calc the sum of the array and divide it by it length
+        return numArr.reduce(sum)/length
+    }
+
+    median(numArr) {
+        // Handle Validation
+
+        //Sort the original array
+        numArr.sort();
+
+        const mid = Math.floor((numArr.length - 1) /2);
+
+        // Checking if the number array is even or odd
+        const isEven = numArr.length % 2 === 0;
+
+        // calculating the median
+        return isEven ? (numArr[mid - 1] + numArr[mid]) / 2 : numArr[mid];
+
+    }
+
+    mode(numArr) {
+        let occurrences = {};
+        let mostOccurred = null;
+        let maxNum = 0;
+      
+        for (let val of numArr) {
+          if (!occurrences[val]) {
+            occurrences[val] = 1;
+          } else {
+            occurrences[val] += 1;
+          }
+        }
+      
+        for (const [k, v] of Object.entries(occurrences)) {
+          if (v > maxNum) {
+            maxNum = v;
+            mostOccurred = k;
+          }
+        }
+      
+        let mode = Number(mostOccurred);
+      
+        return Number(mode);
+    }
+
+}
+
+module.exports = new mathController();
---
+++ routes/math.js
@@ -0,0 +1,11 @@
+/**
+ * Math Routes for complex calculations such as mean, median, mode, etc
+ */
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/mean')
+
+module.exports = router;
+
